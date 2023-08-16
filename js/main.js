e37081121c0a33cf78b2ef20c590e7b2751b4ea1
Pecoden <90499770+0WEBID0@users.noreply.github.com>
2023-08-16 22:21:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+setTimeout(function() {
+    var element = document.getElementById('img100');
+    element.style.display = 'block';
+  }, 1000);
+
+  setTimeout(function() {
+    var element = document.getElementById('img101');
+    element.style.display = 'block';
+  }, 2000);
+
+  setTimeout(function() {
+    var element = document.getElementById('img102');
+    element.style.display = 'block';
+  }, 4000);
